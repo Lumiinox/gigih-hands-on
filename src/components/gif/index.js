dd83a8f3f4d5be2bf0dd6e4772608c10b4852aa9
Lumiinox <maximillian.michael2000@gmail.com>
2022-03-30 23:13:41 +0700

    Hands On Module 3 Session 2

--- src/components/gif/index.js
+++ src/components/gif/index.js
@@ -1,12 +1,14 @@
 const GifComponent = (props) =>{
     return (
-        <tr>
-            <td>{props.title}</td>
-            <td><img src={props.url}></img></td>
-            <td>ID: {props.id}</td>
-            <td>Upload Date: {props.date}</td>
-            <td>Rating: {props.rating}</td>
-        </tr>
+        <>
+            <tr>
+                <td>{props.title}</td>
+                <td><img src={props.url}></img></td>
+                <td>ID: {props.id}</td>
+                <td>Upload Date: {props.date}</td>
+                <td>Rating: {props.rating}</td>
+            </tr>
+        </>
     );    
 }
 
